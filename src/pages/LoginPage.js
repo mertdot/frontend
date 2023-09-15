4d58aa669bf7e90f1b25e508610133a7097e9a41
Mert Ozturk <muharremmertozturk@gmail.com>
2023-09-15 10:00:01 +0300

    14/09/2023
    ...

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -5,35 +5,55 @@ import { login } from '../api/apiCalls';
 class LoginPage extends Component {
     state = {
         username: null,
-        password: null
+        password: null,
+        state: null
     }
 
     onChange = event => {
         const { name, value } = event.target;
         this.setState({
-            [name]: value
+            [name]: value,
+            error: null
         })
     }
 
-    onClickLogin = event => {
+    onClickLogin = async event => {
         event.preventDefault();
         const { username, password } = this.state;
         const creds = {
             username,
             password
         }
-        login(creds);
+        this.setState({
+            error: null
+        })
+        try{
+            await login(creds);
+        } catch(apiError){
+            this.setState({
+                error: apiError.response.data.message
+            })
+        }
     }
 
     render() {
+
+        const { username, password, error} = this.state;
+
+        const buttonEnabled = username && password;
+
         return (
             <div className='container'>
                 <form>
                     <h1 className='text-center'>Login</h1>
                     <Input label="Username" name="username" onChange = {this.onChange} ></Input>
                     <Input label="Password" name="password" type="password" onChange = {this.onChange} ></Input>
+                    {this.state.error && <div className="alert alert-danger" role="alert">{this.state.error}</div>}
                     <div className='text-center'>
-                    <button className='btn btn-primary' onClick={this.onClickLogin}>Login</button>
+                    <button className='btn btn-primary' 
+                    onClick={this.onClickLogin}
+                    disabled={!buttonEnabled}
+                    >Login</button>
                     </div>
                 </form>
             </div>
